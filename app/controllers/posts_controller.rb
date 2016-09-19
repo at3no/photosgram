0003a58a8d358a182eb20d910cfb1b0889f79e06
山能敦志 <AtsushiYamanou@yamanouatsushi-no-MacBook-Pro.local>
2016-09-19 15:30:30 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    if params[:back]
+      @post = Post.new(posts_params)
+    else
+      @post = Post.new
+    end
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿を作成しました！"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(posts_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def confirm
+    @post = Post.new(posts_params)
+    render :new if @post.invalid?
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :content, :image, :image_cache)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+end
